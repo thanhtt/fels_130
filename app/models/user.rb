fa646cba73b601d2406de816fa0e6990f80f7b2e
thanhtt <tran.tien.thanh@framgia.com>
2016-04-27 10:21:40 +0700

    Sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ActiveRecord::Base
   has_many :active_relationships, class_name: Relationship.name,
-    foreign_key: :follower_id:, dependent: :destroy
+    foreign_key: :follower_id, dependent: :destroy
   has_many :passive_relationships, class_name: Relationship.name,
     foreign_key: :followed_id, dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :lessons, dependent: :destroy
   has_many :activities, dependent: :destroy
+
+  before_save {self.email = email.downcase}
+  validates :name,  presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
 end
