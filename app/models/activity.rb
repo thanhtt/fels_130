06aa0988e29190b0a260817cde7467fe3f582bd3
nminhduc <nguyen.minh.duc@framgia.com>
2016-05-06 08:50:48 +0700

    view user_show

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,11 @@
 class Activity < ActiveRecord::Base
   belongs_to :user
+
+  def load_lesson
+    Lesson.find_by id: self.target_id
+  end
+
+  def load_relationship
+    Relationship.find_by id: self.target_id
+  end
 end
