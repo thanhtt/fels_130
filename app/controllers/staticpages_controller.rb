a27d988a069bcd0100cac4cd7a47ff660d7ba4b4
nminhduc <nguyen.minh.duc@framgia.com>
2016-05-09 17:02:51 +0700

    create homepage

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,4 +1,6 @@
 class StaticpagesController < ApplicationController
   def home
+    @feed_items = Activity.activities(current_user).
+      paginate page: params[:page]
   end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -8,4 +8,11 @@ class Activity < ActiveRecord::Base
   def load_user
     User.find_by id: self.target_id
   end
+
+  QUERRY_ACTIVITIES = "user_id in (select followed_id from
+    relationships where follower_id = ?) or user_id = ?"
+
+  scope :activities, ->user_id do
+    where(QUERRY_ACTIVITIES, user_id, user_id).order created_at: :desc
+  end
 end
